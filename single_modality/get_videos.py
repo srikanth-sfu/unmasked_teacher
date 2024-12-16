b1a0b324ff49c8358840b2b71c4e47ea25c32869
Srikanth Muralidharan <smuralid@cdr2603.int.cedar.computecanada.ca>
2024-12-16 12:30:01 -0800

    filter videos bug fix

--- single_modality/get_videos.py
+++ single_modality/get_videos.py
@@ -5,7 +5,7 @@ import shutil
 import string
 
 # Specify the folder containing CSV files and file pattern
-file_pattern = "video_splits/hmdb*.csv"  # Update the path accordingly
+file_pattern = "video_splits/ucf*.csv"  # Update the path accordingly
 csv_files = glob.glob(file_pattern)
 
 # Initialize an empty list to store the data
@@ -23,7 +23,7 @@ videos = glob.glob('data/*/*')
 
 # Define the source directory and destination directory
 source_dir = "data"  # Replace with your source folder path
-destination_dir = "hmdb51"  # Replace with your destination folder path
+destination_dir = "ucf"  # Replace with your destination folder path
 
 # Ensure destination directory exists
 os.makedirs(destination_dir, exist_ok=True)
@@ -32,25 +32,30 @@ os.makedirs(destination_dir, exist_ok=True)
 video_ids = set([x[0] for x in data])
 # Iterate through the source directory recursively
 found = set()
-allowed_chars = string.ascii_letters + string.digits + "-_."
-filtered = [(x,'v_'+ "".join([y for y in os.path.basename(x) if y in allowed_chars])) for x in videos if 'Dhan_Dhana' in x]
+allowed_chars = string.ascii_letters + string.digits + "-_.!"
+#filtered = [(x,'v_'+ "".join([y for y in os.path.basename(x) if y in allowed_chars])) for x in videos if 'Sovereigns' in x]
+#print(filtered)
 for root, _, files in os.walk(source_dir):
     for file in files:
         # Extract the file name without extension
         file_id, ext = os.path.splitext(file)
         # Check if the file name matches any of the video IDs
-        
-        if 'v_'+file_id in video_ids:
-            found.add('v_'+file_id)
+        if file_id in video_ids:
+            source_path = os.path.join(root, file)
+            destination_path = os.path.join(destination_dir, file_id+ext)
+            # Copy file to destination
+            shutil.copy(source_path, destination_path)
+            print(f"Copied: {source_path} -> {destination_path}")
+            found.add(file_id)
         else:
             # Construct full paths
             file_id_dst = "".join([x for x in file_id if x in allowed_chars])
-            if 'v_'+file_id in videos:
-                found.add('v_'+file_id)
+            if file_id_dst in video_ids:
+                found.add(file_id_dst)
                 source_path = os.path.join(root, file)
-                destination_path = os.path.join(destination_dir, 'v_'+file_id_dst)
+                destination_path = os.path.join(destination_dir, file_id_dst+ext)
                 # Copy file to destination
                 shutil.copy(source_path, destination_path)
                 print(f"Copied: {source_path} -> {destination_path}")
 
-print(len(video_ids-found))
+print((video_ids-found))
