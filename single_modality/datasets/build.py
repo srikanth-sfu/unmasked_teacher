6ee757c3ea49f757f8053d9166c1510b90310a97
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-12-22 11:04:31 -0500

    add ucf_hmdb

--- single_modality/datasets/build.py
+++ single_modality/datasets/build.py
@@ -125,6 +125,7 @@ def build_dataset(is_train, test_mode, args):
             new_height=256,
             new_width=320,
             args=args)
+        print(nb_classes)
         nb_classes = args.nb_classes
 
         if 'sparse' in args.data_set:
