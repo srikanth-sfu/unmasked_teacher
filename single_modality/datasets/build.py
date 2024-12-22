d8d12f383ff05a4d69212081bc99d11618a27000
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-12-22 11:13:04 -0500

    split

--- single_modality/datasets/build.py
+++ single_modality/datasets/build.py
@@ -150,7 +150,8 @@ def build_dataset(is_train, test_mode, args):
             short_side_size=args.short_side_size,
             new_height=256,
             new_width=320,
-            args=args)
+            args=args,
+            split=',')
         
         nb_classes = args.nb_classes
     
