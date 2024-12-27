d3d41d3d703a3292bbc93576617dd56c84b1766f
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-12-27 11:48:57 -0500

    latest code

--- single_modality/scripts/baseline/finetune/ucf_hmdb.sh
+++ single_modality/scripts/baseline/finetune/ucf_hmdb.sh
@@ -14,10 +14,10 @@
 cd $SLURM_TMPDIR
 cp -r /project/def-mpederso/smuralid/envs/umt.zip . 
 unzip -qq umt.zip
-module load opencv/4.9.0
+module load StdEnv/2023  intel/2023.2.1
 module load python/3.10.13
 module load rust/1.76.0
-module load cuda/12.2
+module load cuda/11.8
 source umt/bin/activate
 mkdir data && cd data
 cp -r /project/def-mpederso/smuralid/datasets/ucf_hmdb .
