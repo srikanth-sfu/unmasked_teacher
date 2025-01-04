88bc16a20266a820a4a489379718ae0669906e6f
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-03 20:13:21 -0500

    dbg

--- single_modality/models/clip.py
+++ single_modality/models/clip.py
@@ -134,14 +134,12 @@ class VisionTransformer(nn.Module):
 
     def forward(self, x, mask=None):
         x = self.conv1(x)  # shape = [*, width, grid, grid]
-        print("Inside teacher 1", x.shape)
         N, C, T, H, W = x.shape
         x = x.permute(0, 2, 3, 4, 1).reshape(N * T, H * W, C)
 
         x = torch.cat([self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)  # shape = [*, grid ** 2 + 1, width]
         x = x + self.positional_embedding.to(x.dtype)
         x = self.ln_pre(x)
-        print("Inside teacher", x.shape)
         if mask is not None:
             cls_tokens = x[:, :1, :]
             x = x[:, 1:]
@@ -154,6 +152,7 @@ class VisionTransformer(nn.Module):
 
         x = x.permute(1, 0, 2)  # NLD -> LND
         x, attn = self.transformer(x)
+        print("Inside teacher", x.shape)
 
         K = x.shape[0]
         x = self.ln_post(x[:, 1:, :, :])  # [HW, NT, C]
