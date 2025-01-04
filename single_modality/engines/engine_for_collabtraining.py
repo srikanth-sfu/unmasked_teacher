a1ee22b05fa4bf808175ae841914a43bdb933c03
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-03 20:49:20 -0500

    dbg

--- single_modality/engines/engine_for_collabtraining.py
+++ single_modality/engines/engine_for_collabtraining.py
@@ -129,10 +129,10 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                 norm_clip = norm_clip.reshape(B,-1,norm_clip.shape[-1]).mean(dim=1)
                 clip_output = (norm_clip @ clip_label_embedding.T)#.reshape(B,-1).mean(dim=-1).squeeze(0)
                 clip_label_conf = nn.functional.softmax(clip_output, dim=-1)
-                clip_labels = clip_label_conf.max(-1)[-1]
+                clip_label_conf, clip_labels = clip_label_conf.max(-1)
                 src_output = model(samples_tgt)
                 src_encoder_labels_conf = nn.functional.softmax(src_output,dim=-1)
-                src_encoder_labels = src_encoder_labels_conf.max(-1)[-1]
+                src_encoder_labels_conf, src_encoder_labels = src_encoder_labels_conf.max(-1)
                 target_labels, target_mask, target_conf = combine_labels(clip_labels, clip_label_conf, src_encoder_labels, src_encoder_labels_conf, threshold=0.1)
         
             
