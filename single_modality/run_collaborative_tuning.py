b861db5f9593a6bedd11b5afd47c3f7210ddf60c
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-06 17:18:16 -0500

    max acc tgt

--- single_modality/run_collaborative_tuning.py
+++ single_modality/run_collaborative_tuning.py
@@ -545,7 +545,7 @@ def main(args, ds_init):
 
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
-    max_accuracy_src = 0.0
+    max_accuracy_src, max_accuracy_tgt = 0.0, 0.0
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             data_loader_train_src.sampler.set_epoch(epoch)
