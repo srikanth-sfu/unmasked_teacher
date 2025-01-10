c08ef11b34312204480b4e306320bc79b9c9cd28
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-10 09:27:42 -0500

    dbg

--- single_modality/test_clip.py
+++ single_modality/test_clip.py
@@ -10,41 +10,37 @@ def loadvideo_decord(sample, prefix=None, video_ext='.avi', sample_rate_scale=1,
     fname = sample
     fname = os.path.join(prefix, fname)
 
-    try:
-        vr = VideoReader(fname, width=224, height=224,
-                        num_threads=1, ctx=cpu(0))
+    vr = VideoReader(fname, width=224, height=224,
+                    num_threads=1, ctx=cpu(0))
+    print(vr)
 
-        # handle temporal segments
-        clip_len, frame_sample_rate, num_segment, mode = 16, 4, 1, "validation" 
-        converted_len = int(clip_len * frame_sample_rate)
-        seg_len = len(vr) // num_segment
+    # handle temporal segments
+    clip_len, frame_sample_rate, num_segment, mode = 16, 4, 1, "validation" 
+    converted_len = int(clip_len * frame_sample_rate)
+    seg_len = len(vr) // num_segment
 
 
-        all_index = []
-        for i in range(num_segment):
-            if seg_len <= converted_len:
-                index = np.linspace(0, seg_len, num=seg_len // frame_sample_rate)
-                index = np.concatenate((index, np.ones(clip_len - seg_len // frame_sample_rate) * seg_len))
-                index = np.clip(index, 0, seg_len - 1).astype(np.int64)
+    all_index = []
+    for i in range(num_segment):
+        if seg_len <= converted_len:
+            index = np.linspace(0, seg_len, num=seg_len // frame_sample_rate)
+            index = np.concatenate((index, np.ones(clip_len - seg_len // frame_sample_rate) * seg_len))
+            index = np.clip(index, 0, seg_len - 1).astype(np.int64)
+        else:
+            if mode == 'validation':
+                end_idx = (seg_len - converted_len) // 2
             else:
-                if mode == 'validation':
-                    end_idx = (seg_len - converted_len) // 2
-                else:
-                    end_idx = np.random.randint(converted_len, seg_len)
-                str_idx = end_idx - converted_len
-                index = np.linspace(str_idx, end_idx, num=clip_len)
-                index = np.clip(index, str_idx, end_idx - 1).astype(np.int64)
-            index = index + i*seg_len
-            all_index.extend(list(index))
+                end_idx = np.random.randint(converted_len, seg_len)
+            str_idx = end_idx - converted_len
+            index = np.linspace(str_idx, end_idx, num=clip_len)
+            index = np.clip(index, str_idx, end_idx - 1).astype(np.int64)
+        index = index + i*seg_len
+        all_index.extend(list(index))
 
-        all_index = all_index[::int(sample_rate_scale)]
-        vr.seek(0)
-        buffer = vr.get_batch(all_index).asnumpy()
-        return buffer
-    except:
-        print("Colab file, video cannot be loaded by decord: ", fname)
-        os._exit(1)
-        return []
+    all_index = all_index[::int(sample_rate_scale)]
+    vr.seek(0)
+    buffer = vr.get_batch(all_index).asnumpy()
+    return buffer
 
 if __name__ == "__main__":
     import random
