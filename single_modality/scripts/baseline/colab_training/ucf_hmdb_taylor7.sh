8244e7ee64f7486306430d462dc6fe5f59f07279
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-15 13:23:33 -0500

    branch name

--- single_modality/scripts/baseline/colab_training/ucf_hmdb_taylor7.sh
+++ single_modality/scripts/baseline/colab_training/ucf_hmdb_taylor7.sh
@@ -3,7 +3,7 @@ source $SLURM_TMPDIR/envs/umt_taylor7/bin/activate
 export CUDA_HOME=/home/ens/smuralidharan/cuda_local
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
-git checkout baseline_12
+git checkout tubelet_umt_colabtrainingonly
 cd single_modality
 
 bash exp/colab_training/ucf_hmdb/b16_ptk710_ftucfhmdb_f8_res224_taylor.sh
