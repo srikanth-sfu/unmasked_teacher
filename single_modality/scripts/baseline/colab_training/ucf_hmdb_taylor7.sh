ed73f26fdfc7ee66c1035d7dcb2462b8cfa62795
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-15 13:23:12 -0500

    script

--- single_modality/scripts/baseline/colab_training/ucf_hmdb_taylor7.sh
+++ single_modality/scripts/baseline/colab_training/ucf_hmdb_taylor7.sh
@@ -1,40 +1,9 @@
-#!/bin/bash
-# ---------------------------------------------------------------------
-# SLURM script for job resubmission on our clusters. 
-# ---------------------------------------------------------------------
-#SBATCH --job-name=baseline_colabtrain
-#SBATCH --account=rrg-mpederso
-#SBATCH --mem-per-cpu=96G
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:v100l:4
-#SBATCH --time=0-03:00
-#SBATCH -o /home/smuralid/error/tubelets/umt_tubelet/colabtrainonly/ucf_hmdb/slurm-%j.out  # Write the log on scratch
-#SBATCH -e /home/smuralid/error/tubelets/umt_tubelet/colabtrainonly/ucf_hmdb/slurm-%j.err
-
 export SLURM_TMPDIR=/storage/smuralidharan
-source $SLURM_TMPDIR/envs/umt/bin/activate
-mkdir data && cd data
-cd $SLURM_TMPDIR/data/ucf_hmdb
-unzip -qq ucf101.zip
-unzip -qq hmdb51.zip
-cd $SLURM_TMPDIR
-
-git clone git@github.com:srikanth-sfu/unmasked_teacher.git
-cd unmasked_teacher
-git checkout tubelet_umt_colabtrainingonly
+source $SLURM_TMPDIR/envs/umt_taylor7/bin/activate
+export CUDA_HOME=/home/ens/smuralidharan/cuda_local
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
+git checkout baseline_12
 cd single_modality
 
-
-timeout 176m bash exp/colab_training/ucf_hmdb/b16_ptk710_ftucfhmdb_f8_res224_taylor.sh
- 
-if [ $? -eq 124 ]; then
-  echo "The script timed out after ${MAX_HOURS} hour(s). Restarting..."
-  # Call the script itself again with the same configuration
-  cd $SLURM_SUBMIT_DIR
-  sbatch scripts/baseline/colab_training/ucf_hmdb.sh 
-  # scontrol requeue $SLURM_JOB_ID
-else
-  echo "Script completed before timeout"
-  # Exit or perform any other necessary cleanup
-fi
-
+bash exp/colab_training/ucf_hmdb/b16_ptk710_ftucfhmdb_f8_res224_taylor.sh
