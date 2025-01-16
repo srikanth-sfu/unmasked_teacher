9a1a3614e9f89834a6fb1fdcdfb43dfcf700a688
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-15 19:41:19 -0500

    add moco loss

--- single_modality/engines/engine_for_collabtraining.py
+++ single_modality/engines/engine_for_collabtraining.py
@@ -157,7 +157,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
             bool_masked_pos[pos1, pos2] = 0
             bool_masked_pos = bool_masked_pos.view(B, -1).to(torch.bool)
         src_tubelet = model(src_tubelet, return_feats=True)
-        #moco_loss = moco(model.module, src_tubelet, tgt_tubelet)["nce_loss"].mean()
+        moco_loss = moco(model.module, src_tubelet, tgt_tubelet)["nce_loss"].mean()
         with torch.cuda.amp.autocast():
             outputs_clip = model(clip_videos, mask=bool_masked_pos)
             if target_mask.type(torch.int).sum() > 0: 
@@ -166,7 +166,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
             else:
                 loss_target = torch.tensor(0.)
 
-        loss = loss+loss_target#+moco_loss
+        loss = loss+loss_target+moco_loss
         loss_value = loss.item()
         
         if not math.isfinite(loss_value):
