39e93959bb05239e72dc8e73dfe924dfb04cc2b8
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-17 23:07:08 -0500

    ktaridft

--- single_modality/run_class_finetuning.py
+++ single_modality/run_class_finetuning.py
@@ -331,7 +331,7 @@ def main(args, ds_init):
         args.model,
         pretrained=False,
         num_classes=args.nb_classes,
-        all_frames=args.num_frames, #* args.num_segments,
+        all_frames=args.num_frames, * args.num_segments,
         tubelet_size=args.tubelet_size,
         use_learnable_pos_emb=args.use_learnable_pos_emb,
         fc_drop_rate=args.fc_drop_rate,
