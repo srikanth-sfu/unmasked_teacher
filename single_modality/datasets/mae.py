2abfbd9aba9ca2383b64cde358404367eff1c0f8
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-01-21 14:49:02 -0500

    dbg

--- single_modality/datasets/mae.py
+++ single_modality/datasets/mae.py
@@ -197,9 +197,7 @@ class VideoMAE(torch.utils.data.Dataset):
                 Resize(256, interpolation='bilinear'),
                 CenterCrop(size=(224, 224)),
             ])
-            print(np.array(images[0]).shape)
-            os._exit(1)
-            raw_images = [data_transform(np.array(x)) for x in data_transform(images)]
+            raw_images = [data_transform(np.array(x)) for x in images]
             raw_images = torch.from_numpy(raw_images)
             print(process_data.shape, raw_images.shape)
             os._exit(1)
