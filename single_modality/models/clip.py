20d196974964d8cacfab97dabe441a32039208b1
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-02-03 13:54:00 -0500

    clip pth

--- single_modality/models/clip.py
+++ single_modality/models/clip.py
@@ -5,7 +5,9 @@ from collections import OrderedDict
 import torch
 from torch import nn
 
-MODEL_PATH = '/home/ens/smuralidharan/clip_checkpoints/umt/clip_visual_encoder'
+MODEL_PATH = '/home/ens/smuralidharan/checkpoints/umt/clip_visual_encoder'
+if "SLURM_TMPDIR" in os.environ:
+    MODEL_PATH = '/project/def-mpederso/smuralid/checkpoints/umt/clip_visual_encoder'
 _MODELS = {
     # extracted from OpenAI, see extract_clip
     "ViT-B/16": os.path.join(MODEL_PATH, "vit_b16.pth"),
