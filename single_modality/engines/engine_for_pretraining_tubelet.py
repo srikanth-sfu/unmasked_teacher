35a62771a09779986d20712f9f06244293244234
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-02-13 15:02:10 -0500

    dbg

--- single_modality/engines/engine_for_pretraining_tubelet.py
+++ single_modality/engines/engine_for_pretraining_tubelet.py
@@ -60,7 +60,7 @@ def train_one_epoch(
         preds_dbg = (100.0 * out_dbg @ text_embed.type(torch.float32).T).softmax(dim=-1)
         _, preds_dbg = preds_dbg.topk(1)
         cur = (preds_dbg.cpu().numpy() == targets[:,0].numpy())
-        print(cur, preds_dbg.cpu().numpy().shape)
+        print(cur, cur.sum(), preds_dbg.cpu().numpy().shape)
         metric_logger.update(lr=10)
         metric_logger.update(min_lr=10)
         continue
