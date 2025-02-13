7a13cdd4f0246abea6d79160e872292b1fa48220
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-02-13 15:24:42 -0500

    dbg

--- single_modality/engines/engine_for_pretraining_tubelet.py
+++ single_modality/engines/engine_for_pretraining_tubelet.py
@@ -78,10 +78,6 @@ def train_one_epoch(
         acc_dbg, total_dbg = acc_dbg+cur.item(), total_dbg+preds_dbg.shape[0] 
         acc_pc2 = 100*acc_dbg/total_dbg
         print("ACCS",acc_pc1,acc_pc2)
-        metric_logger.update("acc1", acc_pc1)
-        metric_logger.update("acc2", acc_pc2)
-        
-
 
         continue
         num_rows = 7
