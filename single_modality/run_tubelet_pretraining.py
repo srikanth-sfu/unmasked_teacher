bda9b7b4c34e71539c53a36196c3b7aa1d3b93ef
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-02-17 13:32:51 -0500

    dp

--- single_modality/run_tubelet_pretraining.py
+++ single_modality/run_tubelet_pretraining.py
@@ -397,11 +397,11 @@ def main(args):
         moco_model_without_ddp = moco.module
         teacher_model = torch.nn.parallel.DistributedDataParallel(teacher_model, device_ids=[args.gpu], find_unused_parameters=False)
     else:
-        model = torch.nn.parallel.DataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
-        moco = torch.nn.parallel.DataParallel(moco, device_ids=[args.gpu], find_unused_parameters=True)
+        model = torch.nn.parallel.DataParallel(model, find_unused_parameters=True)
+        moco = torch.nn.parallel.DataParallel(moco, find_unused_parameters=True)
         model_without_ddp = model.module
         moco_model_without_ddp = moco.module
-        teacher_model = torch.nn.parallel.DataParallel(teacher_model, device_ids=[args.gpu], find_unused_parameters=False)
+        teacher_model = torch.nn.parallel.DataParallel(teacher_model, find_unused_parameters=False)
     
     optimizer = create_optimizer(args, model_without_ddp, additional_params=moco_model_without_ddp)
     loss_scaler = NativeScaler()
