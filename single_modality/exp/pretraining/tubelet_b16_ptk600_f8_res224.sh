87044ee9459fba5d1a74f8d18f9df223af6e8839
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-02-19 16:41:37 -0500

    increase bs

--- single_modality/exp/pretraining/tubelet_b16_ptk600_f8_res224.sh
+++ single_modality/exp/pretraining/tubelet_b16_ptk600_f8_res224.sh
@@ -35,7 +35,7 @@ python -u -m torch.distributed.launch --nproc_per_node 4 run_umt_pretraining.py 
     --tubelet_size 1 \
     --lr 1.5e-4 \
     --drop_path 0.1 \
-    --batch_size 24 \
+    --batch_size 48 \
     --num_segments 8 \
     --num_frames 8 \
     --sampling_rate 1 \
