5139d8d366c69ada6428643b0c5b573591edeed4
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-02-20 15:17:39 -0500

    num epochs

--- single_modality/run_tubelet_pretraining.py
+++ single_modality/run_tubelet_pretraining.py
@@ -416,7 +416,7 @@ def main(args):
     print(f"Mask ratio: {args.mask_ratio}")
     start_time = time.time()
     for epoch in range(args.start_epoch, args.epochs):
-        if epoch >= 14:
+        if epoch >= 15:
             break
         if args.distributed:
             data_loader_train.sampler.set_epoch(epoch)
