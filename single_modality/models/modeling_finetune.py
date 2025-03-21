abdc3272372f71926aa674523e9785cd1ba9c7d3
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-03-21 16:39:16 -0400

    dim

--- single_modality/models/modeling_finetune.py
+++ single_modality/models/modeling_finetune.py
@@ -209,28 +209,29 @@ class InceptionModule(nn.Module):
     def __init__(self, in_channels, out_channels, name):
         super(InceptionModule, self).__init__()
 
-        self.b0 = Unit3D(in_channels=in_channels, output_channels=out_channels[0], kernel_shape=[1, 1, 1], padding=0,
+        self.b0 = Unit3D(in_channels=in_channels, output_channels=out_channels, kernel_shape=[1, 1, 1], padding=0,
                          name=name+'/Branch_0/Conv3d_0a_1x1')
-        self.b1a = Unit3D(in_channels=in_channels, output_channels=out_channels[1], kernel_shape=[1, 1, 1], padding=0,
-                          name=name+'/Branch_1/Conv3d_0a_1x1')
-        self.b1b = Unit3D(in_channels=out_channels[1], output_channels=out_channels[2], kernel_shape=[3, 3, 3],
-                          name=name+'/Branch_1/Conv3d_0b_3x3')
-        self.b2a = Unit3D(in_channels=in_channels, output_channels=out_channels[3], kernel_shape=[1, 1, 1], padding=0,
+        # self.b1a = Unit3D(in_channels=in_channels, output_channels=out_channels[1], kernel_shape=[1, 1, 1], padding=0,
+        #                   name=name+'/Branch_1/Conv3d_0a_1x1')
+        # self.b1b = Unit3D(in_channels=out_channels[1], output_channels=out_channels[2], kernel_shape=[3, 3, 3],
+        #                   name=name+'/Branch_1/Conv3d_0b_3x3')
+        self.b2a = Unit3D(in_channels=in_channels, output_channels=out_channels, kernel_shape=[1, 1, 1], padding=0,
                           name=name+'/Branch_2/Conv3d_0a_1x1')
-        self.b2b = Unit3D(in_channels=out_channels[3], output_channels=out_channels[4], kernel_shape=[3, 3, 3],
+        self.b2b = Unit3D(in_channels=in_channels, output_channels=out_channels, kernel_shape=[3, 3, 3],
                           name=name+'/Branch_2/Conv3d_0b_3x3')
-        self.b3a = MaxPool3dSamePadding(kernel_size=[3, 3, 3],
-                                stride=(1, 1, 1), padding=0)
-        self.b3b = Unit3D(in_channels=in_channels, output_channels=out_channels[5], kernel_shape=[1, 1, 1], padding=0,
-                          name=name+'/Branch_3/Conv3d_0b_1x1')
+        # self.b3a = MaxPool3dSamePadding(kernel_size=[3, 3, 3],
+        #                         stride=(1, 1, 1), padding=0)
+        # self.b3b = Unit3D(in_channels=in_channels, output_channels=out_channels[5], kernel_shape=[1, 1, 1], padding=0,
+        #                   name=name+'/Branch_3/Conv3d_0b_1x1')
         self.name = name
 
     def forward(self, x):    
         b0 = self.b0(x)
-        b1 = self.b1b(self.b1a(x))
+        #b1 = self.b1b(self.b1a(x))
         b2 = self.b2b(self.b2a(x))
-        b3 = self.b3b(self.b3a(x))
-        return torch.cat([b0,b1,b2,b3], dim=1)
+        #b3 = self.b3b(self.b3a(x))
+        return torch.cat([b0,b2], dim=1)
+        #return torch.cat([b0,b1,b2,b3], dim=1)
 
 
 class Block(nn.Module):
@@ -253,7 +254,7 @@ class Block(nn.Module):
             self.gamma_2 = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
         else:
             self.gamma_1, self.gamma_2 = None, None
-        self.i3d_module = InceptionModule(dim,dim,"i3d_"+name)
+        self.i3d_module = InceptionModule(dim,dim//2,"i3d_"+name)
 
     def forward(self, x):
         y = self.i3d_module(x)
