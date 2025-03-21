bebf3a428261705c82e0ee7a7d6702a2c305a0c8
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-03-21 16:48:41 -0400

    dbg

--- single_modality/models/modeling_finetune.py
+++ single_modality/models/modeling_finetune.py
@@ -140,6 +140,7 @@ class Unit3D(nn.Module):
             
     def forward(self, x):
         # compute 'same' padding
+        print(x.size())
         (batch, channel, t, h, w) = x.size()
         #print t,h,w
         out_t = np.ceil(float(t) / float(self._stride[0]))
