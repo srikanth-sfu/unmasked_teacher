4eac372be121ccb08f4db6c4be20709644b2a6f0
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-03-26 10:43:54 -0400

    rm dbg

--- single_modality/engines/engine_for_pretraining_umt.py
+++ single_modality/engines/engine_for_pretraining_umt.py
@@ -99,7 +99,6 @@ def train_one_epoch(
             loss_pixel = torch.zeros(1).type_as(outputs_clip).to(outputs_clip.device)
             # align CLIP
             if clip_loss_type == 'l2':
-                print(outputs_clip.shape, norm_clip.shape)
                 B, n_feat = norm_clip.shape[1], norm_clip.shape[-1]
                 loss_clip1 = (2 - 2 * (outputs_clip[:,2,:196] * norm_clip[-1].reshape(B, 8, 196, n_feat).mean(dim=1)).sum(dim=-1)).mean()
                 loss_clip2 = (2 - 2 * (outputs_clip[:,1,:392] * norm_clip[-3].reshape(B, 8, 196, n_feat)[:,::4].reshape(B, 392, n_feat)).sum(dim=-1)).mean()
