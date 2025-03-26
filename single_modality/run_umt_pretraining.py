250f1f8bd031274037f4a09ceefd0d65cf0fae34
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-03-26 00:15:04 -0400

    load

--- single_modality/run_umt_pretraining.py
+++ single_modality/run_umt_pretraining.py
@@ -405,7 +405,7 @@ def main(args):
     wd_schedule_values = utils.cosine_scheduler(args.weight_decay, args.weight_decay_end, args.epochs, num_training_steps_per_epoch)
     print("Max WD = %.7f, Min WD = %.7f" % (max(wd_schedule_values), min(wd_schedule_values)))
 
-    utils.auto_load_model(args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
+    #utils.auto_load_model(args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
     torch.cuda.empty_cache()
     print(f"Start training for {args.epochs} epochs")
     print(f"Mask ratio: {args.mask_ratio}")
