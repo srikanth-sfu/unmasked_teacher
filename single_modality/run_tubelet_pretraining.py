543818c306b1c2afb53c94654598a3da513540f3
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-04-03 00:14:10 -0400

    add vidfocalnet

--- single_modality/run_tubelet_pretraining.py
+++ single_modality/run_tubelet_pretraining.py
@@ -241,8 +241,8 @@ def main(args):
 
     #model = get_model(args)
     #moco_model = get_model(args)
-    model = InceptionI3d(400, in_channels=3)
-    moco_model = InceptionI3d(400, in_channels=3)
+    model = build_model(type="videofocalnet_tiny", pretrained=True)
+    moco_model = build_model(type="videofocalnet_tiny", pretrained=True)
     for param in moco_model.parameters():
         param.requires_grad = False
     print("Tubelet size = %s" % str(args.tubelet_size))
