f2f804126e20aa3410a8bc6ec826f10340d4a565
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-04-03 00:48:32 -0400

    batch

--- single_modality/exp/pretraining/tubeletonly_t16_ptarid_f8_res224.sh
+++ single_modality/exp/pretraining/tubeletonly_t16_ptarid_f8_res224.sh
@@ -35,7 +35,7 @@ python -u -m torch.distributed.launch --nproc_per_node 4 run_tubelet_pretraining
     --tubelet_size 1 \
     --lr 1.5e-4 \
     --drop_path 0.1 \
-    --batch_size 8 \
+    --batch_size 4 \
     --num_segments 8 \
     --num_frames 12 \
     --sampling_rate 1 \
