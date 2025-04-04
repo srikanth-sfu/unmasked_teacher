c2e65f4c731a40524ea5118051429e8cc6af149c
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-04-04 07:53:41 -0400

    x3d

--- single_modality/models/__init__.py
+++ single_modality/models/__init__.py
@@ -3,3 +3,4 @@ from .modeling_finetune import vit_base_patch16_224, vit_base_patch16_384, vit_l
 from .modeling_pretrain_umt import pretrain_umt_base_patch16_224, pretrain_umt_large_patch16_224 
 from .modeling_pretrain import pretrain_videomae_base_patch16_224, pretrain_videomae_large_patch16_224, pretrain_videomae_huge_patch16_224
 from .build import build_model
+from .x3d import ResNet
--- single_modality/run_tubelet_pretraining.py
+++ single_modality/run_tubelet_pretraining.py
@@ -241,8 +241,10 @@ def main(args):
 
     #model = get_model(args)
     #moco_model = get_model(args)
-    model = build_model(model_type="videofocalnet_tiny", is_pretrained=True, num_classes=0)
-    moco_model = build_model(model_type="videofocalnet_tiny", is_pretrained=True, num_classes=0)
+    #model = build_model(model_type="videofocalnet_tiny", is_pretrained=True, num_classes=0)
+    #moco_model = build_model(model_type="videofocalnet_tiny", is_pretrained=True, num_classes=0)
+    model = ResNet("M")
+    moco_model = ResNet("M")
     for param in moco_model.parameters():
         param.requires_grad = False
     print("Tubelet size = %s" % str(args.tubelet_size))
