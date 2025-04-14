5e4fdd27a5f60ba0076ab2c2c2c2ff8096d31b50
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-04-14 16:48:32 -0400

    rm dbg

--- single_modality/run_class_finetuning.py
+++ single_modality/run_class_finetuning.py
@@ -337,7 +337,7 @@ def main(args, ds_init):
             prob=args.mixup_prob, switch_prob=args.mixup_switch_prob, mode=args.mixup_mode,
             label_smoothing=args.smoothing, num_classes=args.nb_classes)
 
-    model = InceptionI3d(args.nb_classes+1, in_channels=3)
+    model = InceptionI3d(args.nb_classes, in_channels=3)
 
     
     checkpoint = torch.load(args.finetune, map_location='cpu')
