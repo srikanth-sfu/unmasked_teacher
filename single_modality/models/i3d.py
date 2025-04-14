be0c688060d97104dd24f5f7560b55daa53e7bef
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-04-14 16:37:50 -0400

    train logits

--- single_modality/models/i3d.py
+++ single_modality/models/i3d.py
@@ -325,14 +325,13 @@ class InceptionI3d(nn.Module):
                 x = self._modules[end_point](x) # use _modules to work with dataparallel
 
         x = self.avg_pool(x)
-        return x
-
-        # x = self.logits(self.dropout(self.avg_pool(x)))
-        # if self._spatial_squeeze:
-        #     logits = x.squeeze(3).squeeze(3)
-        # logits is batch X time X classes, which is what we want to work with
-        # return logits
-        
+        if self._num_classes > 0:
+            x = self.logits(self.dropout(self.avg_pool(x)))
+            if self._spatial_squeeze:
+                logits = x.squeeze(3).squeeze(3)
+            #logits is batch X time X classes, which is what we want to work with
+            return logits
+            
 
     def extract_features(self, x):
         for end_point in self.VALID_ENDPOINTS:
